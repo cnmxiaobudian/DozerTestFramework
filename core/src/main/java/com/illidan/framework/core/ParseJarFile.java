f06624ea4c946c36970db3d22e987574a3de6490
cnmxiaobudian <353202532@qq.com>
2015-03-20 20:55:00 +0800

    解析上传的jar以及load
    解析上传的jar以及load

--- core/src/main/java/com/illidan/framework/core/ParseJarFile.java
+++ core/src/main/java/com/illidan/framework/core/ParseJarFile.java
@@ -1,6 +1,8 @@
 package com.illidan.framework.core;
 
 import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
@@ -14,7 +16,7 @@ import java.util.jar.JarFile;
  */
 public class ParseJarFile
 {
-	//可以解析jarFile的jar文件
+	//可以解析jarFile的jar文件，并将jar中的文件过滤为class返回
 	public List<String> parseJar(String jarPath) throws IOException
 	{
 		List<String> result = new ArrayList<String>();
@@ -27,24 +29,50 @@ public class ParseJarFile
 			String clazz = jarEntry.getName();
 			if(clazz.endsWith("class") && !clazz.contains("Builder"))
 			{
-				result.add(clazz.replace("/", "."));
+				result.add(clazz.replace("/", ".").replace(".class", ""));
 			}
 		}
 		
 		return result;
 	}
 	
-	//TODO 有一个非常严重的问题待解决：目前虽然可以解析到JarFile的class类，但是没办法new出来，因为包不再当前工程中。
-	//TODO 判断class是一个javaBean
+	//加载jar文件
+	//TODO 只需要加载jar文件即可，不需要new出来，然后调用dozer测试框架的class，进行测试
+	public void loadClass(String jarPath) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException
+	{
+		//解析jar文件，获得class
+		List<String> clazzs = parseJar(jarPath);
+		
+		//手动加载jar
+		URL url = new URL("file:" + jarPath);
+		URLClassLoader urlClassLoader = new URLClassLoader(new URL[]{url});
+		
+		//newInstance
+		for(String clazz : clazzs)
+		{
+			Class class1 = urlClassLoader.loadClass(clazz);
+			if(!class1.isInterface())
+			{
+				class1.newInstance();
+			}
+		}
+	}
+	
 	public static void main(String[] args)
 	{
 		ParseJarFile parseJarFile = new ParseJarFile();
 		try
 		{
-			//Object obj = Class.forName("com.taobao.ad.ultron.dal.vo.AccountItemVO");
 			List<String> clazzs = parseJarFile.parseJar("../core/src/main/resources/jarFile/ultron-dal.jar");
+			URL url = new URL("file:../core/src/main/resources/jarFile/ultron-dal.jar");
+			URLClassLoader urlClassLoader = new URLClassLoader(new URL[]{url});
 			for(String clazz : clazzs)
 			{
+				Class class1 = urlClassLoader.loadClass(clazz);
+				if(!class1.isInterface())
+				{
+					class1.newInstance();
+				}
 				Object object = Class.forName(clazz);
 				System.out.println(object);
 				System.out.println(clazz);
